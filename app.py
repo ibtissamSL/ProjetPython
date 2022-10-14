c96168f6bc11aa6466ce77baaf189f4793abcb0f
ibtissamSL <sallybetty20@gmail.com>
2022-10-14 19:28:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ import plotly.express as px
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.io as pio
+import base64
+#import pygame
 
 app = Dash(__name__)
 
-print("ghghg")
+#image=pygame.image.load("C:\Users\ibtis\OneDrive\Bureau\me\BettyM2_SISE\projet Python\drive-download-20221014T165149Z-001\logo.png")
+#app.layout = 
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
@@ -19,13 +22,6 @@ print("ghghg")
 
 #df = pd.read_csv("C:/Users/ibtis/Downloads/train.csv")
 
-'''df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")'''
 
 df = pd.read_csv(r"C:/Users/ibtis/OneDrive/Bureau/me/BettyM2_SISE/projet Python/env1/files/train.csv", sep=";")
 
@@ -57,29 +53,37 @@ barchart = px.bar(
 '''barchart.title('Nombre de personnes par vague')
 barchart.xlabel('N° de la vague')
 barchart.ylabel('Nombre de participants')'''
-pio.show(barchart)
+#pio.show(barchart)
 
 #sunburst
-fig3 = px.sunburst(
+'''fig3 = px.sunburst(
     data_frame = df,
     path =['from','field','race'],
-    color = 'from'
-)
-
+    color = 'from',
+    color_discrete_sequence=px.colors.qualitative.Pastel,
+    maxdepth=-1
+)'''
 
 
 app.layout = html.Div(children=[
+    #html.Img(src=app.get_asset_url('https://drive.google.com/file/d/1bYdHUNetNhSvCfCRCqhfzBHCKgrVUIpG/view?usp=sharing')),
+    
     html.H1(children='EasyDate - AI match'),
-
-    html.Div(children='''
-        Prédire si l’amour va opérer entre deux personnes.
-    '''),
-
+    html.Div(children='Prédire si l’amour va opérer entre deux personnes'),
     dcc.Graph(
         id='example-graph',
-        figure=barchart
-    )
+        figure=fig
+    ),
+    html.Div(html.Img(src=app.get_asset_url('https://drive.google.com/file/d/1bYdHUNetNhSvCfCRCqhfzBHCKgrVUIpG/view?usp=sharing'))) #, style={'height':'2%', 'width':'2%'}
 ])
+
+test_png = 'https://drive.google.com/file/d/1bYdHUNetNhSvCfCRCqhfzBHCKgrVUIpG/view?usp=sharing'
+test_base64 = base64.b64encode(open(test_png, 'rb').read()).decode('ascii')
+
+
+app.layout = html.Div([
+    html.Img(src='data:image/png;base64,{}'.format(test_base64)),
+    ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
