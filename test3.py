0352945ce81820bbe5e474b9a2d47bb43fba7b69
ibtissamSL <sallybetty20@gmail.com>
2022-10-16 16:27:37 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,22 +1,22 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-import os 
-from PIL import Image
-import numpy as np
-import plotly.express as px
-from dash import Dash, dcc, html
+import pandas as pd
 
-#os.chdir(os.path.dirname(os.path.abspath(__file__)))
+df_pred = pd.read_csv(r'C:/Users/ibtis/OneDrive/Documents/GitHub/ProjetM2Pythion/prediction.csv')#,sep=","
+#print(df_pred.head())
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+#df_pred_1 = df_pred.groupby()
+#df_pred_1 = df_pred.groupby(["target"]).agg( {'iid' : 'count'}).sort_values(by ="iid", ascending =False)
+df_pred_1 = df_pred.groupby(['target'],as_index=False).agg( {'target' : 'count'})#.sum()
+print(df_pred_1.head())
 
-app.layout = html.Div(
-    [
-        html.Img(src ='C:/Users/lhaton/Desktop/Dash/venv1/Scripts/logo.png')
-])
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+
+
+
+
+
+
+
+
+
+
+
