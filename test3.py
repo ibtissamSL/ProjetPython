c2ce38ea97d5f2f2d5c085cbbb0ae12358111f1d
ibtissamSL <sallybetty20@gmail.com>
2022-10-16 19:29:47 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -6,7 +6,9 @@ df_pred = pd.read_csv(r'C:/Users/ibtis/OneDrive/Documents/GitHub/ProjetM2Pythion
 #df_pred_1 = df_pred.groupby()
 #df_pred_1 = df_pred.groupby(["target"]).agg( {'iid' : 'count'}).sort_values(by ="iid", ascending =False)
 df_pred_1 = df_pred.groupby(['target'],as_index=False).agg( {'target' : 'count'})#.sum()
-print(df_pred_1.head())
+#print(df_pred_1.head())
+
+print(df_pred)
 
 
 
