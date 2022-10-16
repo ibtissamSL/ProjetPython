b7ad27d7cb2361c4eaf0db303a6e55399c57e4ee
ibtissamSL <sallybetty20@gmail.com>
2022-10-16 19:29:15 +0200

    Create predict.py

---
+++ apps/predict.py
@@ -0,0 +1,23 @@
+from ctypes import alignment
+import plotly.graph_objects as go
+from dash import Dash
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+
+app.layout = html.Div([
+
+    html.Div(children=[
+    #html.Img(src=app.get_asset_url('https://drive.google.com/file/d/1bYdHUNetNhSvCfCRCqhfzBHCKgrVUIpG/view?usp=sharing')),
+    
+    html.H1(children='EasyDate - AI match',style={'textAlign': 'center', 'color': '#FFC0CB'}),
+    html.H2(children='Prédire si l’amour va opérer entre deux personnes',style={'textAlign': 'center', 'color': '#8B008B'}),
+    ]),
+    
+    ])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
