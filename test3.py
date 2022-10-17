14d684bddcde3c2384bf3094ef779901926c0157
ibtissamSL <sallybetty20@gmail.com>
2022-10-17 11:01:33 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,14 +1,219 @@
+# '''import pandas as pd
+
+# df_pred = pd.read_csv(r'C:/Users/ibtis/OneDrive/Documents/GitHub/ProjetM2Pythion/prediction.csv')#,sep=","
+# #print(df_pred.head())
+
+# #df_pred_1 = df_pred.groupby()
+# #df_pred_1 = df_pred.groupby(["target"]).agg( {'iid' : 'count'}).sort_values(by ="iid", ascending =False)
+# df_pred_1 = df_pred.groupby(['target'],as_index=False).agg( {'target' : 'count'})#.sum()
+# #print(df_pred_1.head())
+
+# print(df_pred)
+# #'''
+
+
+
+
+#Most basic circular barplot with Python and Matplotlib
+#https://www.python-graph-gallery.com/circular-barplot-basic
+#création d'un graphique circulaire pour les activités et leur popularité
+
+# Import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#définition 
+
+# Initialize plot with polar coordinates.
+plt.subplot(111, polar=True);
+plt.bar(x=0, height=10, width=np.pi/2, bottom=5);
+#plt.show()
+
+# import pandas for data wrangling
 import pandas as pd
 
-df_pred = pd.read_csv(r'C:/Users/ibtis/OneDrive/Documents/GitHub/ProjetM2Pythion/prediction.csv')#,sep=","
-#print(df_pred.head())
+# Build a dataset
+df = pd.DataFrame(
+        {
+            'Name': ['item ' + str(i) for i in list(range(1, 51)) ],
+            'Value': np.random.randint(low=10, high=100, size=50)
+        })
 
-#df_pred_1 = df_pred.groupby()
-#df_pred_1 = df_pred.groupby(["target"]).agg( {'iid' : 'count'}).sort_values(by ="iid", ascending =False)
-df_pred_1 = df_pred.groupby(['target'],as_index=False).agg( {'target' : 'count'})#.sum()
-#print(df_pred_1.head())
+# Show 3 first rows
+df.head(3)
 
-print(df_pred)
+# set figure size
+plt.figure(figsize=(20,10))
+
+# plot polar axis
+ax = plt.subplot(111, polar=True)
+
+# remove grid
+plt.axis('off')
+
+# Set the coordinates limits
+upperLimit = 100
+lowerLimit = 30
+
+# Compute max and min in the dataset
+max = df['Value'].max()
+
+# Let's compute heights: they are a conversion of each item value in those new coordinates
+# In our example, 0 in the dataset will be converted to the lowerLimit (10)
+# The maximum will be converted to the upperLimit (100)
+slope = (max - lowerLimit) / max
+heights = slope * df.Value + lowerLimit
+
+# Compute the width of each bar. In total we have 2*Pi = 360°
+width = 2*np.pi / len(df.index)
+
+# Compute the angle each bar is centered on:
+indexes = list(range(1, len(df.index)+1))
+angles = [element * width for element in indexes]
+angles
+
+# Draw bars
+bars = ax.bar(
+    x=angles, 
+    height=heights, 
+    width=width, 
+    bottom=lowerLimit,
+    linewidth=2, 
+    edgecolor="white")
+
+#plt.show()
+
+
+# initialize the figure
+plt.figure(figsize=(20,10))
+ax = plt.subplot(111, polar=True)
+plt.axis('off')
+
+# Draw bars
+bars = ax.bar(
+    x=angles, 
+    height=heights, 
+    width=width, 
+    bottom=lowerLimit,
+    linewidth=2, 
+    edgecolor="white",
+    color="#61a4b2",
+)
+
+# little space between the bar and the label
+labelPadding = 4
+
+# Add labels
+for bar, angle, height, label in zip(bars,angles, heights, df["Name"]):
+
+    # Labels are rotated. Rotation must be specified in degrees :(
+    rotation = np.rad2deg(angle)
+
+    # Flip some labels upside down
+    alignment = ""
+    if angle >= np.pi/2 and angle < 3*np.pi/2:
+        alignment = "right"
+        rotation = rotation + 180
+    else: 
+        alignment = "left"
+
+    # Finally add the labels
+    ax.text(
+        x=angle, 
+        y=lowerLimit + bar.get_height() + labelPadding, 
+        s=label, 
+        ha=alignment, 
+        va='center', 
+        rotation=rotation, 
+        rotation_mode="anchor") 
+
+#plt.show()
+
+
+# import pandas for data wrangling
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# # Build a dataset
+# df = pd.DataFrame(
+#         {
+#             'Name': ['item ' + str(i) for i in list(range(1, 51)) ],
+#             'Value': np.random.randint(low=10, high=100, size=50)
+#         })
+
+# # Reorder the dataframe
+# df = df.sort_values(by=['Value'])
+
+#définition de notre df
+df = pd.read_csv(r'C:/Users/ibtis/OneDrive/Documents/GitHub/ProjetM2Pythion/submissions.csv',sep=';')
+print(df.head())
+
+# initialize the figure
+plt.figure(figsize=(20,10))
+ax = plt.subplot(111, polar=True)
+plt.axis('off')
+
+# Constants = parameters controling the plot layout:
+upperLimit = 100
+lowerLimit = 30
+labelPadding = 4
+
+# Compute max and min in the dataset
+max = df['Value'].max()
+
+# Let's compute heights: they are a conversion of each item value in those new coordinates
+# In our example, 0 in the dataset will be converted to the lowerLimit (10)
+# The maximum will be converted to the upperLimit (100)
+slope = (max - lowerLimit) / max
+heights = slope * df.Value + lowerLimit
+
+# Compute the width of each bar. In total we have 2*Pi = 360°
+width = 2*np.pi / len(df.index)
+
+# Compute the angle each bar is centered on:
+indexes = list(range(1, len(df.index)+1))
+angles = [element * width for element in indexes]
+angles
+
+# Draw bars
+bars = ax.bar(
+    x=angles, 
+    height=heights, 
+    width=width, 
+    bottom=lowerLimit,
+    linewidth=2, 
+    edgecolor="white",
+    color="#61a4b2",
+)
+
+# Add labels
+for bar, angle, height, label in zip(bars,angles, heights, df["Name"]):
+
+    # Labels are rotated. Rotation must be specified in degrees :(
+    rotation = np.rad2deg(angle)
+
+    # Flip some labels upside down
+    alignment = ""
+    if angle >= np.pi/2 and angle < 3*np.pi/2:
+        alignment = "right"
+        rotation = rotation + 180
+    else: 
+        alignment = "left"
+
+    # Finally add the labels
+    ax.text(
+        x=angle, 
+        y=lowerLimit + bar.get_height() + labelPadding, 
+        s=label, 
+        ha=alignment, 
+        va='center', 
+        rotation=rotation, 
+        rotation_mode="anchor") 
+
+#plt.show()
+
 
 
 
