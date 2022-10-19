7111e1176cef59c8a3aef23465cbf3ac716065c5
Samibgh <78345903+Samibgh@users.noreply.github.com>
2022-10-19 13:47:10 +0200

    change

--- app.py
+++ app.py
@@ -1,103 +1,473 @@
+#this is a copy of the test2.py
 
-#for the multiple pages app...
+"""
+This app creates a simple sidebar layout using inline style arguments and the
+dbc.Nav component.
+
+dcc.Location is used to track the current location, and a callback uses the
+current location to render the appropriate page content. The active prop of
+each NavLink is set automatically according to the current pathname. To use
+this feature you must install dash-bootstrap-components >= 0.11.0.
+
+For more details on building multi-page Dash applications, check out the Dash
+documentation: https://dash.plot.ly/urls
+"""
+from dataclasses import field
 import dash
+import dash_bootstrap_components as dbc
+from dash import Input, Output, dcc, html, State
+import pandas as pd
+import plotly.graph_objs as go
+import io
+import base64
+import plotly.express as px
+import matplotlib.pyplot as plt
+from Activite import ActSub,ActTrain,Nuagemot , FreqDate, FreqSortie,carSub,carTrain,filedSub,filedTrain,EtSub,EtTrain
 
-# meta_tags are required for the app layout to be mobile responsive
-app = dash.Dash(__name__, suppress_callback_exceptions=True,
-                meta_tags=[{'name': 'viewport',
-                            'content': 'width=device-width, initial-scale=1.0'}]
-                )
+age = ["26-35 ans","0-25 ans","plus de 35 ans"]
+genre = ["Femme", "Homme"]
+app = dash.Dash(external_stylesheets=[dbc.themes.MATERIA])
 server = app.server
 
+urlSub= r'C:/Users/samib/Documents/GitHub/ProjetM2Pythion/datasets/submissionsClean.csv'
+df = pd.read_csv(urlSub,sep = ",",header=0)
+df["iid_pid"] = df["iid_pid"].astype(int)
+
+#url= 'C:\Users\samib\Documents\GitHub\ProjetM2Pythion\datasets\DataClean.csv.py?raw=true'
+Train = pd.read_csv(r'C:/Users/samib/Documents/GitHub/ProjetM2Pythion/datasets/DataClean.csv',sep = ",",header=0)
+
+urlPred= 'https://github.com/Samibgh/ProjetM2Pythion/blob/main/prediction.csv?raw=true'
+pred = pd.read_csv(r'C:/Users/samib/Documents/GitHub/ProjetM2Pythion/datasets/prediction.csv',sep = ",",header=0)
+
+# urlPred= 'https://github.com/Samibgh/ProjetM2Pythion/blob/main/prediction.csv?raw=true'
+# pred = pd.read_csv(urlPred,sep = ",",header=0)
+
+urlXsub = "https://github.com/Samibgh/ProjetM2Pythion/blob/main/submissions.csv"
+XSub = pd.read_csv(r'C:/Users/samib/Documents/GitHub/ProjetM2Pythion/datasets/submissions.csv', sep=";")
+
+dfinal = XSub.merge(pred, on="iid_pid", how = 'inner')
+
+dfinal = dfinal[['iid_pid','iid','pid','target']]
+
+list_of_single_column = list(dfinal['iid'])
+mylist = list(dict.fromkeys(list_of_single_column))
+
+# #define a function that will return the pid who matched (where target =='1')
+# dfinal.loc[dfinal['target'] == 1]
+# #les rencontres qui ont fini par un match
+
+# dfinal.loc[dfinal['target'] == 1][['iid_pid']]
+# #si on insère l'iid, 
+# dfinal.loc[dfinal['target'] == 1].loc[dfinal['iid'] == 491][['pid']]
 
 
-"<<<<<<< HEAD"
-
-from dash import Dash, html, dcc
-import plotly.express as px
-import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.io as pio
-import base64
-#import pygame
-
-app = Dash(__name__)
-
-#image=pygame.image.load("C:\Users\ibtis\OneDrive\Bureau\me\BettyM2_SISE\projet Python\drive-download-20221014T165149Z-001\logo.png")
-#app.layout = 
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
 
 
-#df = pd.read_csv("C:/Users/ibtis/Downloads/train.csv")
+df = pred.merge(df, on="iid_pid", how = 'left')
 
-url2 = 'https://github.com/Samibgh/ProjetM2Pythion/blob/main/submissionsClean.csv?raw=true'
-df = pd.read_csv(url2,sep = ",",header=0)
+def recode(age) :
+    if age<25.0  :
+        return "0-25 ans"
+    elif age >25.0 and age<35.0 :
+        return "26-35 ans"
+    elif age >35.0 :
+        return "plus de 35 ans"
 
-print(df)
+df['age_recode'] = df['age'].apply(recode)
+df_age = df.drop_duplicates('iid')['age_recode'].value_counts()
 
-fig = px.bar(df, x="sports", y="iid_pid", color="gender", barmode="group")
+Train['age_recode'] = Train['age'].apply(recode)
+Train_age = Train.drop_duplicates('iid')['age_recode'].value_counts()
 
-#fig2 = plt.pie(df["gender"].value_counts(),labels=round(df["gender"].value_counts(normalize=True)*100,2))
-fig2 = px.pie(df, values='gender', names='gender')
-#plt.title('Répartition des genres')
-#plt.show()
 
-#nombre de personnes par vague
-df_ = df.drop_duplicates("iid").groupby(["wave"]).agg( {'iid' : 'count'}).sort_values(by ="iid", ascending =False)
-df_.insert(0,'wave',df_.index.tolist())
-df_=df_[['wave','iid']]
-#print(df_)
-barchart = px.bar(
-    data_frame=df_,
-    x='wave',
-    y='iid',
-    #color="gender",
-    opacity=0.9,
-    orientation="v",
-    barmode="overlay",
-    title="Nombre de personnes par vague",
-    labels='N° de la vague',
-    color_discrete_map={1:"yellow", 0:"blue"}
-    #...
+
+#fig_logo2
+fig_logo2 = go.Figure()
+fig_logo2.add_layout_image(
+    dict(
+        source="https://raw.githubusercontent.com/Samibgh/ProjetM2Pythion/main/images/logo2.png",
+       xref="paper", yref="paper",
+       x=1, y=1.05,
+       sizex=0.2, sizey=0.2,
+        xanchor="right", yanchor="bottom"
+    )
 )
-'''barchart.title('Nombre de personnes par vague')
-barchart.xlabel('N° de la vague')
-barchart.ylabel('Nombre de participants')'''
-#pio.show(barchart)
 
-#sunburst
-'''fig3 = px.sunburst(
-    data_frame = df,
-    path =['from','field','race'],
-    color = 'from',
-    color_discrete_sequence=px.colors.qualitative.Pastel,
-    maxdepth=-1
-)'''
+##############################      fig_im    begin     ######################################
+# Create figure
+fig_im = go.Figure()
 
+# Constants
+img_width = 1600
+img_height = 800
+scale_factor = 0.5#0.5
 
-app.layout = html.Div(children=[
-    #html.Img(src=app.get_asset_url('https://drive.google.com/file/d/1bYdHUNetNhSvCfCRCqhfzBHCKgrVUIpG/view?usp=sharing')),
+# '''# Add invisible scatter trace.
+# # This trace is added to help the autoresize logic work.
+# fig_im.add_trace(
+#     go.Scatter(
+#         x=[0, img_width * scale_factor],
+#         y=[0, img_height * scale_factor],
+#         mode="markers",
+#         marker_opacity=0
+#     )
+# )'''
+
+# Configure axes
+fig_im.update_xaxes(
+    visible=False,
+    range=[0, img_width * scale_factor]
+)
+
+fig_im.update_yaxes(
+    visible=False,
+    range=[0, img_height * scale_factor],
+    # the scaleanchor attribute ensures that the aspect ratio stays constant
+    scaleanchor="x"
+)
+
+# Add image
+fig_im.add_layout_image(
+    dict(
+        x=0,
+        sizex=img_width * scale_factor,
+        y=img_height * scale_factor,
+        sizey=img_height * scale_factor,
+        xref="x",
+        yref="y",
+        opacity=1.0,
+        layer="below",
+        sizing="stretch",
+        #source="https://raw.githubusercontent.com/michaelbabyn/plot_data/master/bridge.jpg")
+        source="https://raw.githubusercontent.com/Samibgh/ProjetM2Pythion/main/images/logo.png"),
+        #xanchor = "center", #Sets the anchor for the x position. "left" | "center" | "right"
+        #yanchor = "middle", #Sets the anchor for the y position. "top" | "middle" | "bottom"
+        
+)
+
+# Configure other layout
+fig_im.update_layout(
+    width=img_width * scale_factor,
+    height=img_height * scale_factor,
+    margin={"l": 0, "r": 0, "t": 0, "b": 0},
+    #align="center"
     
-    html.H1(children='EasyDate - AI match'),
-    html.Div(children='Prédire si l’amour va opérer entre deux personnes'),
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
+)
+
+##############################      fig_im    end    ######################################
+
+
+
+##############################      fig_logo2_2    begin     ######################################
+# Create figure
+fig_logo2_2 = go.Figure()
+fig_logo2_2.add_layout_image(
+    dict(
+        source="https://raw.githubusercontent.com/Samibgh/ProjetM2Pythion/main/images/logo2.png",
+       xref="paper", yref="paper",
+       x=1, y=1.05,
+       sizex=0.2, sizey=0.2,
+        xanchor="right", yanchor="bottom"
+    )
+)
+# Constants
+img_width = 260#160
+img_height = 180#90
+scale_factor = 0.5
+
+# '''# Add invisible scatter trace.
+# # This trace is added to help the autoresize logic work.
+# fig_im.add_trace(
+#     go.Scatter(
+#         x=[0, img_width * scale_factor],
+#         y=[0, img_height * scale_factor],
+#         mode="markers",
+#         marker_opacity=0
+#     )
+# )'''
+
+# Configure axes
+fig_logo2_2.update_xaxes(
+    visible=False,
+    range=[0, img_width * scale_factor]
+)
+
+fig_logo2_2.update_yaxes(
+    visible=False,
+    range=[0, img_height * scale_factor],
+    # the scaleanchor attribute ensures that the aspect ratio stays constant
+    scaleanchor="x"
+)
+
+# Add image
+fig_logo2_2.add_layout_image(
+    dict(
+        x=0,
+        sizex=img_width * scale_factor,
+        y=img_height * scale_factor,
+        sizey=img_height * scale_factor,
+        xref="x",
+        yref="y",
+        opacity=1.0,
+        layer="below",
+        sizing="stretch",
+        #source="https://raw.githubusercontent.com/michaelbabyn/plot_data/master/bridge.jpg")
+        source="https://raw.githubusercontent.com/Samibgh/ProjetM2Pythion/main/images/logo2.png"),
+        #xanchor = "center", #Sets the anchor for the x position. "left" | "center" | "right"
+        #yanchor = "middle", #Sets the anchor for the y position. "top" | "middle" | "bottom"
+        
+)
+
+# Configure other layout
+fig_logo2_2.update_layout(
+    width=img_width * scale_factor,
+    height=img_height * scale_factor,
+    margin={"l": 0, "r": 0, "t": 0, "b": 0},
+    #align="center"
+    
+)
+
+##############################      fig_logo2_2    end     ####################################################
+
+#Appel des fonctions
+ActS = ActSub(df)
+ActT = ActTrain(Train)
+Nuagemot(ActT,ActS, 'tab20b','tab20b' )
+FreqSortie(df)
+FreqDate(df)
+carriereTrain = carTrain(Train)
+carriereSub = carSub(df)
+fiTrain = filedTrain(Train)
+filSUb = filedSub(df)
+etnTrain = EtTrain(Train)
+etnSub = EtSub(df)
+
+tab = html.Div(
+    [
+        html.H2("Speed Dating", style = {"font-size" : 50, "text-align": "center"}),
+        html.Hr(),
+        dbc.Tabs(
+            [
+                dbc.Tab(label = "Acceuil", tab_id="Acceuil"),
+                dbc.Tab(label = "DashBoard", tab_id="DashBoard"),
+                dbc.Tab(label = "Prediction", tab_id="prediction"),
+            ],
+            id = "onglet", active_tab="Acceuil"
+        ),
+    ], style = {"color": "black"}
+)
+
+
+CountMatch = pd.DataFrame(pred["target"].value_counts())
+CountMatch = CountMatch.reset_index()
+CountMatch.columns = ["Match","target"]
+
+content = dbc.Container( 
+
+    [
+        
+        html.Div([
+  
+        html.Div(children=[
+            dcc.Graph(
+            id='graph_fig_logo2_2',
+            figure=fig_logo2_2#style={'textAlign': 'center'}#'width':'75%', 'margin':25, 
+        ),],),
+
+        html.Div(children=[
+            #html.Img(src=app.get_asset_url('https://drive.google.com/file/d/1bYdHUNetNhSvCfCRCqhfzBHCKgrVUIpG/view?usp=sharing')),
+    
+            #html.H1(children='EasyDate - AI match',style={'textAlign': 'center', 'color': '#7FDBFF'}),
+            #html.Div(children='Prédire si l’amour va opérer entre deux personnes'),
+    
+            html.H1(children='EasyDate - AI match',style={'textAlign': 'center', 'color': '#FFC0CB'}),
+            html.H2(children='Prédire si l’amour va opérer entre deux personnes',style={'textAlign': 'center', 'color': '#8B008B'}),
+            #dcc.Graph(
+            #   id='graph_fig_im',
+            #  figure=fig_im,
+            #     ),
+   
+        ]),
+
+
+        html.Div(children=[
+        dcc.Graph(
+            id='graph_fig_im',
+            figure=fig_im#style={'textAlign': 'center'}#'width':'75%', 'margin':25, 
+            #"width": "100%", "display": "flex", 
+        ),],style = {'margin-left' : '350px'}),
+                        
+    ],
+        id= "active_accueil"
     ),
-    html.Div(html.Img(src=app.get_asset_url('https://drive.google.com/file/d/1bYdHUNetNhSvCfCRCqhfzBHCKgrVUIpG/view?usp=sharing'))) #, style={'height':'2%', 'width':'2%'}
-])
+    html.Div([
 
-test_png = 'https://drive.google.com/file/d/1bYdHUNetNhSvCfCRCqhfzBHCKgrVUIpG/view?usp=sharing'
-test_base64 = base64.b64encode(open(test_png, 'rb').read()).decode('ascii')
+        #Liste déroulante pour sélection du fichier Train ou Submission
+         html.Div([
+            html.Br(),
+            dcc.Dropdown(['Train', 'Submissions'], id='dropdown', value='Train'),
+         ],
+         style = {'width' : '350px'}
+         ),
+
+        #RadioItems pour sélection des données à afficher
+        html.Div(children=[
+        dcc.RadioItems(options=['career_c','age','gender','race','field_cd'],id='my-radio-btn',value='career_c'),],style = {'margin-top' : '35px','margin-left' : '35px'}),
+
+        #affichage du graphe après sélection des 3 paramètres 
+        html.Br(),
+        html.Div(dcc.Graph(id='graph')),
+        html.Br(),
+        html.Br(),
+        html.Div([
+            html.H3('Nuage de mots Train'),
+           html.Img(src=r"assets/Trainmots.png",alt="image",style = {'margin-left':30}),
+
+         ], style= {"width" : 600, "display" : "inline-block" }),
+
+         html.Div([
+             html.H3('Nuage de mots Submissions'),
+             html.Img(src=r"assets/Submots.png",alt="image",style = {'margin-right':30})
+         ],  style= {"width" : 600, "display" : "inline-block" })
+    ],
+    id="active_dashboard"
+    ),
+    html.Div([
+        html.Br(),
+
+        html.Br(),
+        html.H4("Répartition des matchs prédis : "),
+        dbc.Table.from_dataframe(CountMatch, striped=True, bordered=True, hover=True),
+        html.Br(),
+
+        html.Div([
+           html.Img(src=r"assets/FreqSortie.png",alt="image",style ={ "text-align" : "center", 'margin-left':30}),
+
+         ], style= {"width" : 600, "display" : "inline-block" , "margin"  : 5}),
+
+         html.Div([
+             html.Img(src=r"assets/FreqDate.png",alt="image",style ={"text-align" : "center" ," margin-right" : 40})
+         ],  style= {"width" : 600, "display" : "inline-block" , "margin"  : 5}),
+
+        #liste déroulante pour les identifiants
+        html.Div([
+            html.Br(),
+            html.H4("Vérification des matchs selon l'id de la personne : "),
+            dcc.Dropdown(options=mylist, id='dropdown_iid', value=537),
+        ],
+        style = {'width' : '350px'}
+        ),
+
+        html.Div([], id = 'tableau'),
+
+    ],
+    id="active_pred"
+    )
+    ]
+ )
+
+app.layout = html.Div([tab, content])
 
 
-app.layout = html.Div([
-    html.Img(src='data:image/png;base64,{}'.format(test_base64)),
+@app.callback([Output("active_accueil", "style"),Output("active_dashboard", "style"), Output("active_pred", "style")],
+                                                                [Input("onglet","active_tab")])
+def render_tab_content(active_tab):
+
+    on = {"display":"block"}
+
+    off = {"display":"none"}
+
+    if active_tab is not None:
+
+        if active_tab == "Acceuil":
+
+            return on, off, off
+
+        elif active_tab == "DashBoard":
+
+            return off, on, off
+
+        elif active_tab == "prediction":
+
+            return off, off,on
+
+    return "No tab selected"
+
+
+
+
+@app.callback(Output(component_id='graph', component_property='figure'),
+    [Input(component_id='my-radio-btn',component_property='value'),Input(component_id='dropdown',component_property='value')])
+
+def update_output(value,value2):
+ 
+    if value2 == "Train":
+        if value=="gender" :
+            fig_pie= px.pie(Train.drop_duplicates('iid')['gender'].value_counts(),values='gender', names='gender',color_discrete_sequence=px.colors.sequential.RdBu) #,values='gender', names='gender'
+            #fig_pie = plt.pie(df.drop_duplicates('iid')['gender'].value_counts(), labels = ["26-35 ans","0-25 ans","plus de 35 ans"]) #,values='gender', names='gender'
+            return fig_pie
+        if value =='age' :
+            fig_pie = px.pie(Train.drop_duplicates('iid')['age_recode'].value_counts(), values='age_recode', names='age_recode',color_discrete_sequence=px.colors.sequential.RdBu)
+            return fig_pie
+        if value == "career_c" : 
+             #html.Img(src=r"assets/carriereTrain.png",alt="image",style ={"text-align" : "center" })
+             #data = [go.Bar( x = list(carriereTrain.index),y = list(carriereTrain.values))]
+             fig_pie={'data': [ {'x': list(carriereTrain.index), 'y': list(carriereTrain.values), 'type': 'bar'},],'layout': {
+            'title': 'Répartition des carrières'
+        }}
+             return fig_pie
+        if value == 'field_cd':
+            fig_pie={'data': [ {'x': list(fiTrain.index), 'y': list(fiTrain.values), 'type': 'bar'},],'layout': {
+            'title': 'Répartition des dommaines de profession'
+        }}
+            return fig_pie
+        if value == 'race':
+            fig_pie={'data': [ {'x': list(etnTrain.index), 'y': list(etnTrain.values), 'type': 'bar'},],'layout': {
+            'title': 'Répartition des éthnies'
+        }}
+            return fig_pie
+  
+        else :
+            fig_pie = px.pie(Train.drop_duplicates('iid')[value].value_counts(), values=value, names=value,color_discrete_sequence=px.colors.sequential.RdBu)
+            return fig_pie
+            
+    else :
+        if value=="gender" :
+            fig_pie= px.pie(df.drop_duplicates('iid')['gender'].value_counts(),values='gender', names='gender',color_discrete_sequence=px.colors.sequential.Viridis) #,values='gender', names='gender'
+            #fig_pie = plt.pie(df.drop_duplicates('iid')['gender'].value_counts(), labels = ["26-35 ans","0-25 ans","plus de 35 ans"]) #,values='gender', names='gender'
+            return fig_pie
+        if value =='age' :
+            fig_pie = px.pie(df.drop_duplicates('iid')['age_recode'].value_counts(), values='age_recode', names='age_recode',color_discrete_sequence=px.colors.sequential.Viridis)
+            return fig_pie
+        if value == "career_c" : 
+             #html.Img(src=r"assets/carriereTrain.png",alt="image",style ={"text-align" : "center" })
+             #data = [go.Bar( x = list(carriereTrain.index),y = list(carriereTrain.values))]
+             fig_pie={'data': [ {'x': list(carriereSub.index), 'y': list(carriereSub.values), 'type': 'bar'},],'layout': {
+            'title': 'Répartition des carrières'
+        }}
+             return fig_pie
+        if value == 'field_cd':
+            fig_pie={'data': [ {'x': list(filSUb.index), 'y': list(filSUb.values), 'type': 'bar'},],'layout': {
+            'title': 'Répartition des domaines de profession'
+        }}
+            return fig_pie
+        if value == 'race':
+            fig_pie={'data': [ {'x': list(etnSub.index), 'y': list(etnSub.values), 'type': 'bar'},],'layout': {
+            'title': 'Répartition des éthnies'
+        }}
+            return fig_pie
+            
+        else :
+            fig_pie = px.pie(df, values=value, names=value,color_discrete_sequence=px.colors.sequential.Viridis)
+            return fig_pie
+
+#callback et fontion pour afficher les match d'un iid
+@app.callback(Output(component_id='tableau', component_property='children'), 
+    [Input(component_id='dropdown_iid',component_property='value')
     ])
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
-
-">>>>>>>ec1f054c67418d684943ba0055b2b633c00603ca"
+def update_output(value):
+    d = pd.DataFrame(dfinal.loc[dfinal['target'] == 1].loc[dfinal['iid'] == value][['pid']])
+    return dbc.Table.from_dataframe(d, striped=True, bordered=True, hover=True,id="tableau")
+if __name__ == "__main__":
+    app.run_server(debug = True)
---
+++ fonction/Machinelearning.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 14 15:07:29 2022
+
+@author: sibghi
+"""
+
+"""###Partie Machine Learning"""
+
+##Importation des modules 
+import pandas
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import f1_score
+from sklearn.metrics import plot_confusion_matrix
+from sklearn import metrics
+
+#chargement des données
+url = 'https://github.com/Samibgh/ProjetM2Pythion/blob/main/DataClean.csv?raw=true'
+url2 = 'https://github.com/Samibgh/ProjetM2Pythion/blob/main/submissionsClean.csv?raw=true'
+url3 = 'https://github.com/Samibgh/ProjetM2Pythion/blob/main/XSubmission.csv?raw=true'
+
+
+data = pandas.read_csv(url, sep = ",",header=0)
+submissions = pandas.read_csv(url2,sep = ",",header=0)
+XSub = pandas.read_csv(url3,sep = ",",header=0)
+
+def MachineLearning(data,submissions,XSub) : 
+
+  #selection des variables avec une correlation positive a match 
+  matrice = data.corr()
+  corrMatch = matrice.match.sort_values(ascending=False)
+  corrMatch = pandas.DataFrame(corrMatch)
+  corrMatch.iloc[:,0][corrMatch.match > 0]
+  dataModele = corrMatch.index
+
+
+  #création de la variable cible 
+  Y=data["match"]
+  #création de la matrice X
+  X=data[dataModele] #filtre avec les variables selectionnees au-dessus
+  X = data.drop(["iid_pid","iid","pid","id","match","wave","dec_o", 'expnum', 'order', 'positin1', 'position', 'round',"IID_PID",'fun_o', 'attr_o', 'shar_o', 'intel_o', 'sinc_o','amb_o',"pf_o_att",	"pf_o_sin",	"pf_o_int",	"pf_o_fun",	"pf_o_amb",	"pf_o_sha","zipcode"],axis = 1)
+
+
+  #On supprime les variables qu'il n'y a pas dans Xsub
+  for k in X.columns : 
+    if (k not in XSub.columns) : 
+      del X[k]
+
+  #separation des fichier train et test 
+  Xtrain, Xtest,ytrain,ytest = train_test_split(X,Y,train_size=0.5,random_state=0,stratify=Y)
+
+  #Over sampling avec SMOTE
+  smote = SMOTE(sampling_strategy=0.5, k_neighbors = 5, random_state = 0)
+  X_train_resampled,y_train_resampled = smote.fit_resample(Xtrain,ytrain)
+
+  #Mise en place du modele 
+  clf = AdaBoostClassifier(n_estimators = 300,random_state=0).fit(X_train_resampled,y_train_resampled)
+  y_pred = clf.predict(Xtest)
+
+
+  #prediction avec submission
+  y_pred2 = clf.predict(XSub)
+  y_pred2 = pandas.DataFrame(y_pred2)
+
+  submissions.iid_pid=submissions.iid_pid.astype(int,0)
+  sub = pandas.DataFrame(submissions.iid_pid)
+
+  sub = pandas.concat([sub,y_pred2], axis=1)
+
+
+  sub.columns = ["iid_pid","target"]
+
+  return (sub)
+
+#exportation des fichiers 
